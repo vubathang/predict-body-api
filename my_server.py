714b2e8d1f750da29897b47b8c6545f6f62fcc70
thangvb <bathangvu@gmail.com>
2023-10-26 13:17:52 +0700

    add func to get img

--- my_server.py
+++ my_server.py
@@ -3,6 +3,8 @@ from flask_cors import CORS, cross_origin
 from sklearn.preprocessing import PolynomialFeatures
 import joblib
 import pandas as pd
+import cv2
+import numpy as np
 import os
 
 poly = PolynomialFeatures(degree=2)
@@ -19,6 +21,31 @@ app = Flask(__name__)
 # Apply Flask CORS
 CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
+
+# http://127.0.0.1/image
+@app.route('/image', methods=['POST'])
+@cross_origin(origins='*', headers=['Content-Type', 'Authorization'])
+
+def upload_image():
+    image_file = request.files['image']
+    height = request.form['height']
+    weight = request.form['weight']
+    image_np = np.frombuffer(image_file.read(), np.uint8)
+    image = cv2.imdecode(image_np, cv2.IMREAD_COLOR)
+    gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    cv2.imwrite('gray_image.jpg', gray_image)
+    cv2.imshow('Gray Image', gray_image)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+    if os.path.exists('gray_image.jpg'):
+      try:
+          os.remove('gray_image.jpg')
+      except Exception as e:
+          print(f"Lỗi xảy ra khi xóa tệp: {e}")
+    return jsonify({
+       'status': 'success'
+    })
 
 # http://127.0.0.1/predict
 
@@ -38,11 +65,11 @@ def predict():
 
   print(predictions)
   res = {
-     'status': 'success',
-     'data': {
-        **feature,
-        **predictions
-     }
+     'status': 'success'
+    #  'data': {
+    #     **feature,
+    #     **predictions
+    #  }
   }
 
   return jsonify({**res})
