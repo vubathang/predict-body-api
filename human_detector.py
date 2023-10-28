18ba04bab9a208a2e0ca74adc0dabc1ad94adf75
hungbeoqua321 <69001944+hungbeoqua321@users.noreply.github.com>
2023-10-28 08:51:04 +0700

    update

--- human_detector.py
+++ human_detector.py
@@ -9,11 +9,12 @@ def calculateDistance(landmark1, landmark2):
     x1, y1 = landmark1.x, landmark1.y
     x2, y2 = landmark2.x, landmark2.y
 
-    # Calculate the Distance between the two points
-    dis = math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
+    return math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
 
-    # Return the calculated Distance.
-    return dis
+muscle_ratio = 0.023
+
+def calculateCircumference(weight, length): 
+    return length * weight * muscle_ratio
 
 # Khởi tạo đối tượng Mediapipe Pose
 mp_pose = mp.solutions.pose
@@ -26,10 +27,12 @@ mp_drawing = mp.solutions.drawing_utils
 data = []
 
 # Nhập chiều cao thực tế người dùng
-height = float(input("Height (cm): ")) 
+# height = float(input("Height (cm): "))
+height = 163 
+weight = 49
 
 # Đọc ảnh từ tệp ảnh trong thư mục
-file_path = 'front_img.jpg'
+file_path = 'model_predict/front_img.jpg'
 image = cv2.imread(file_path)
 
 # Nhận dạng các điểm mốc trên cơ thể
@@ -41,24 +44,43 @@ if results.pose_landmarks:
     height_pixel = calculateDistance(landmarks[mp_pose.PoseLandmark.RIGHT_EYE.value], 
                                                   landmarks[mp_pose.PoseLandmark.RIGHT_ANKLE.value])
 
-    arm_length_pixel = calculateDistance(landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value], 
-                                         landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value]) + calculateDistance(landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value], 
-                                                  landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value])
+    upper_arm_length_pixel = calculateDistance(landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value], 
+                                               landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value])
     
-    shoulder_length_pixel = calculateDistance(landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value], 
-                                         landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value])
+    arm_length_pixel = upper_arm_length_pixel + calculateDistance(landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value], 
+                                                                                                               landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value])
+    
+    shoulder_length_pixel = calculateDistance(landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value], 
+                                                  landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value])/4
     
     leg_length_pixel = calculateDistance(landmarks[mp_pose.PoseLandmark.LEFT_HIP.value], 
                                          landmarks[mp_pose.PoseLandmark.LEFT_KNEE.value]) + calculateDistance(landmarks[mp_pose.PoseLandmark.LEFT_KNEE.value], 
-                                                  landmarks[mp_pose.PoseLandmark.LEFT_ANKLE.value])
+                                                                                                              landmarks[mp_pose.PoseLandmark.LEFT_ANKLE.value])
 
+    back_build_pixel = abs(landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].y - landmarks[mp_pose.PoseLandmark.LEFT_HIP.value].y)
+    
+    
+    thigh_length_pixel = calculateDistance(landmarks[mp_pose.PoseLandmark.LEFT_HIP.value], 
+                                               landmarks[mp_pose.PoseLandmark.LEFT_KNEE.value])
+    
+    bicep_length_pixel = calculateDistance(landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value], 
+                                               landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value])
+    
     # Tính số đo thực tế từ pixel
     height_ratio = height / height_pixel
     arm_length_cm = height_ratio * arm_length_pixel
     shoulder_length_cm = height_ratio * shoulder_length_pixel
     leg_length_cm = height_ratio * leg_length_pixel
+    back_build_cm = (height_ratio * back_build_pixel) - 11
+    upper_arm_length_cm = height_ratio * upper_arm_length_pixel
+    thigh_length_cm = height_ratio * thigh_length_pixel
+    bicep_length_cm = height_ratio * bicep_length_pixel
+    
+    thigh_circumference = calculateCircumference(weight,thigh_length_cm)
+    bicep_circumference = calculateCircumference(weight,bicep_length_cm)
 
-    data_point = [arm_length_cm, shoulder_length_cm, leg_length_cm]  # Thêm các số đo khác vào danh sách ở đây
+    data_point = [arm_length_cm, shoulder_length_cm, leg_length_cm, back_build_cm, upper_arm_length_cm, thigh_length_cm
+                  ,thigh_circumference, bicep_circumference]
 
     data.append(data_point)
 
@@ -66,17 +88,22 @@ if results.pose_landmarks:
     cv2.putText(image, f"Arm Length: {arm_length_cm:.2f} cm", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
     cv2.putText(image, f"Shoulder Length: {shoulder_length_cm:.2f} cm", (10, 60), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
     cv2.putText(image, f"Leg Length: {leg_length_cm:.2f} cm", (10, 90), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+    cv2.putText(image, f"Back Build: {back_build_cm:.2f} cm", (10, 120), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+    cv2.putText(image, f"Upper arm Length: {upper_arm_length_cm:.2f} cm", (10, 150), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+    cv2.putText(image, f"Thigh Length: {thigh_length_cm:.2f} cm", (10, 180), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+    cv2.putText(image, f"Thigh circumference: {thigh_circumference:.2f} cm", (10, 210), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+    cv2.putText(image, f"Bicep circumference: {bicep_circumference:.2f} cm", (10, 240), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
 
-# # Vẽ các điểm mốc trên cơ thể
-# mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS)
+# Vẽ các điểm mốc trên cơ thể
+mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS)
 
-# # Hiển thị ảnh với các chỉ số cơ thể
-# image = cv2.resize(image, (700, 500))
+# Hiển thị ảnh với các chỉ số cơ thể
+image = cv2.resize(image, (450, 800))
 
-# cv2.imshow('Body Measurements', image)
+cv2.imshow('Body Measurements', image)
 
-# cv2.waitKey(0)
-# cv2.destroyAllWindows()
+cv2.waitKey(0)
+cv2.destroyAllWindows()
 
 
 
